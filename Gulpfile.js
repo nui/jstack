0d2886649b5a55c81af793cc7c2bddc2e2694870
Nui Narongwet <narongwet.m@gmail.com>
2017-02-19 22:37:57 +0700

    gulp webpack with colors

--- Gulpfile.js
+++ Gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('webpack', function (callback) {
         if (err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString({
             // output options
+            colors: true
         }));
         callback();
     });
