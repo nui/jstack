8044d4cffc1e8fcb9956f7fceb9d0ec53aba59d6
Nui Narongwet <narongwet.m@gmail.com>
2017-02-21 00:23:07 +0700

    Less verbose stats

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,11 +7,16 @@ gulp.task('default', function () {
     console.log('Hello');
 });
 
+const webpackStats = {
+    colors: true,
+    chunks: false
+};
+
 gulp.task('start', function (callback) {
     var compiler = webpack(require('./webpack.config'));
 
     new WebpackDevServer(compiler, {
-        stats: {colors: true}
+        stats: webpackStats
         // server and middleware options
     }).listen(8080, "localhost", function (err) {
         if (err) throw new gutil.PluginError("webpack-dev-server", err);
@@ -25,10 +30,7 @@ gulp.task('webpack', function (callback) {
     // run webpack
     webpack(require('./webpack.prod'), function (err, stats) {
         if (err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-            colors: true
-        }));
+        gutil.log("[webpack]", stats.toString(webpackStats));
         callback();
     });
 });
