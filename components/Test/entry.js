20c6782342b43957787b999232524483664b89ff
Nui Narongwet <narongwet.m@gmail.com>
2017-03-12 20:32:53 +0700

    Check for experiment by .isBVariant

--- components/Test/entry.js
+++ components/Test/entry.js
@@ -1,6 +1,7 @@
 import {Experiment} from '../../lib/Experiment';
 
 function test() {
-    // var experiment = new Experiment();
-    experiment.isBVariant('ABC-123');
+    var experiment = new Experiment();
+    if(experiment.isBVariant('ABC-123')) {
+    }
 }
--- plugins/ExperimentPlugin.js
+++ plugins/ExperimentPlugin.js
@@ -1,15 +1,14 @@
 "use strict";
 
 class ExperimentPlugin {
-    constructor() {
-    }
-
     apply(compiler) {
         compiler.plugin('compilation', (compilation, data) => {
             data.normalModuleFactory.plugin('parser', (parser, options) => {
-                parser.plugin("call experiment.isBVariant", (expr) => {
+                parser.plugin("evaluate CallExpression .isBVariant", (expr) => {
                     // you now have a reference to the call expression
-                    console.log(expr.arguments[0].value);
+                    if(expr.arguments && expr.arguments[0].type === 'Literal') {
+                        console.log(`Found experiment : ${expr.arguments[0].value}`);
+                    }
                 });
             });
         });
